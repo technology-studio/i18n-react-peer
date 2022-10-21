ae7de8d755dd2d1dbee5a794e4fe8c0996a9ea8f
Erik Slovak <erik.slovak@technologystudio.sk>
2022-10-21 13:20:00 +0200

    fix: fix typescript issue with i18next translate function

--- src/Api/I18nInit.ts
+++ src/Api/I18nInit.ts
@@ -42,13 +42,15 @@ export const i18nInit = async (): Promise<TFunction> => {
 
   const originalT = i18next.t
   i18next.t = function (...arg: Parameters<typeof originalT>) {
-    const translation: string | null = originalT.apply(this, arg) as string | null
-    return configManager.config.displayLocalizationIssues
-      ? translation
-      : translation?.startsWith('#')
-        ? translation.substr(1)
-        : translation
-  }
+    const translation = originalT(...arg)
+    return typeof translation === 'string'
+      ? configManager.config.displayLocalizationIssues
+        ? translation
+        : translation?.startsWith('#')
+          ? translation.substring(1)
+          : translation
+      : translation
+  } as TFunction
 
   i18next.toNumber = (number: number, format: Record<string, unknown>) => (
     numbro(number).format(format)
